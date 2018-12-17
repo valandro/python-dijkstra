a10e363f9675d3abfc199de9b298ac1f5cbbc469
valandro <lvalandrorocha@hotmail.com>
2018-12-16 22:38:47 -0200

    Number of nodes

--- src/dijsktra.py
+++ src/dijsktra.py
@@ -1,6 +1,23 @@
 from sys import argv
 import re
 
+file = open(argv[1], "r")
+# The greatest node is equal to number of nodes - 1
+greatest_node = -1
+for line in file:
+    # Reading only lines there are not comments
+    if not re.match("//", line):
+        info = line.split(" ")
+        # Find the number of nodes
+        if int(info[0]) > greatest_node:
+            greatest_node = int(info[0])
+        if int(info[1]) > greatest_node:
+            greatest_node = int(info[1])            
+
+# nodes = number max of nodes
+nodes = greatest_node + 1
+file.close()
+
 graph = []
 file = open(argv[1], "r")
 first_line = True
@@ -17,6 +34,6 @@ for line in file:
             arrest = {info[0] + "&" + info[1]: info[2].replace('\n', '')}        
             # Add JSON to graph
             graph.append(arrest)
-
 file.close()
-print("graph = ", graph)
+
+
