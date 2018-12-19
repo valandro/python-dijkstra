88534a8e3cf6004903a096b9cd3fe0ce6464b365
Lucas Valandro <lvalandrorocha@hotmail.com>
2018-12-19 14:51:57 -0200

    Dijkstra algorithm implemented

--- src/dijsktra.py
+++ src/dijsktra.py
@@ -1,6 +1,6 @@
 from sys import argv
 import re
-
+# Infinity cost constant
 INFINITY = 99999
 
 file = open(argv[1], "r")
@@ -16,11 +16,16 @@ for line in file:
         if int(info[1]) > greatest_node:
             greatest_node = int(info[1])            
 
-# nodes = number max of nodes
-nodes = greatest_node + 1
 file.close()
+
+# Total number of nodes
+nodes = greatest_node + 1
 graph = []
+
+# Open the file again
 file = open(argv[1], "r")
+
+# Constants for file manipulation
 first_line = True
 origin = 0
 destiny = 0
@@ -38,6 +43,8 @@ for line in file:
             # Add JSON to graph
             graph.append(arrest)
 file.close()
+
+# Constants for matrix approach
 minimun = INFINITY
 current_node = origin
 col = 1
@@ -46,25 +53,36 @@ cost = 0
 for i in range(len(graph)):
     for node in range(len(graph)):
         n = list(graph[node].keys())[0].split("&")
-        if int(n[0]) == current_node: 
-            # Set origin costs
-            if current_node == origin:
-                matrix[origin][origin] = 0
-                matrix[int(n[0])][int(n[1])] = int(list(graph[node].values())[0])
-                if int(list(graph[node].values())[0]) < minimun:
-                    minimun = int(list(graph[node].values())[0])
+        value = int(list(graph[node].values())[0])
+        if int(n[0]) == current_node:
+            # Check if the node is the origin, then set all columns as correspondent cost
+            if int(n[0]) == origin:
+                matrix[int(n[0])][int(n[0])] = 0
+                matrix[int(n[0])][int(n[1])] = value
+                # Check for the minimun distance and save the target node
+                if value < minimun:
+                    minimun = value
                     col = int(n[1])
             else:
-                print(n[0], n[1], int(list(graph[node].values())[0]), matrix[int(n[0]) - 1][int(n[1])], cost)
-                if int(list(graph[node].values())[0]) > matrix[int(n[0]) - 1][int(n[1])]:
+                # If isn't the origin node, then look back to see if the current cost is smaller than the previous
+                if matrix[int(n[0]) - 1][int(n[1])] < value:
+                    # If it is, than keep the old cost
                     matrix[int(n[0])][int(n[1])] = matrix[int(n[0]) - 1][int(n[1])]
-                    minimun = matrix[int(n[0]) - 1][int(n[1])]
-                    col = int(n[1])
+                    if matrix[int(n[0])][int(n[1])] < minimun:
+                        minimun = matrix[int(n[0])][int(n[1])]
+                        col = int(n[1])
                 else:
-                    print("ae")
-                    matrix[int(n[0])][int(n[1])] = cost + int(list(graph[node].values())[0])
+                    # If isn't, than set the new cost using the actual cost to current node
+                     matrix[int(n[0])][int(n[1])] = cost + value 
+                     if matrix[int(n[0])][int(n[1])] < minimun:
+                        minimun = matrix[int(n[0])][int(n[1])]  
+                        col = int(n[1])
+    # Set new current node
     current_node = col
-    cost += minimun
-    print("cost", cost, minimun)
-    minimun = INFINITY + 1
-print(matrix)
+    cost = minimun
+    if current_node == destiny:
+        break;
+    minimun = INFINITY
+
+print("-- Minimun Cost --")
+print(cost)
